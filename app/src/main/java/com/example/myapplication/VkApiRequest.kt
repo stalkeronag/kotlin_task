241684e80f93b21c2cca2220d87af378dca724e2
Kostan <6PwCnSEPh2NHrEA>
2023-10-27 22:34:28 +0300

    create second_activity.xml

--- app/src/main/java/com/example/myapplication/VkApiRequest.kt
+++ app/src/main/java/com/example/myapplication/VkApiRequest.kt
@@ -18,7 +18,7 @@ class VkApiRequest {
 
     fun GetUserInfoById(users:String): JsonArray {
 
-        val fields = "education,contacts,city"
+        val fields = "contacts,bdate"
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.vk.com/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -33,15 +33,32 @@ class VkApiRequest {
 
     fun ConvertJsonToUser(jsonUsers:JsonArray):MutableList<User> {
         var list_users = mutableListOf<User>()
+        var list_keys = mutableListOf<String>(
+            "bdate",
+            "mobile_phone",
+            "home_phone"
+        )
+
+
         if (jsonUsers != null){
             for(i in 0 until jsonUsers.size()){
                 val jsonObject = jsonUsers[i].asJsonObject
-                jsonObject.get("last_name")
+                for (i in 0 until list_keys.size){
+                    if (jsonObject.get(list_keys[i]) != null){
+                        list_keys[i] = jsonObject.get(list_keys[i]).asString
+                    }
+                    else{
+                        list_keys[i] = "not found"
+                    }
+                }
                 list_users.add(
                     User(
                         id = jsonObject.get("id").asString,
                         last_name = jsonObject.get("last_name").asString,
-                        first_name = jsonObject.get("first_name").asString
+                        first_name = jsonObject.get("first_name").asString,
+                        bdate = list_keys[0],
+                        mobile_phone = list_keys[1],
+                        home_phone = list_keys[2]
                     )
                 )
             }
